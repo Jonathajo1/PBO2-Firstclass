91c0e6f752faa71e401c83f6fb684605914b92cc
Muamarxandrya1 <115987370+Jonathajo1@users.noreply.github.com>
2024-03-19 08:48:14 +0700

    Add files via upload

---
+++ PBO2-Firstclass-master/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+         System.out.println("Hello My Name Muamar");
+        //
+         System.out.println("My Age 21");
+         System.out.println("Ini adalah branch development");
+
+        boolean isAlien = false;
+
+        if (isAlien == false) { /*!isAlien*/
+            System.out.println("im scared of alien");
+            System.out.println("its not an alien!!");
+        }
+
+
+        System.out.println("Who i a'm?");
+
+        }
+    }
